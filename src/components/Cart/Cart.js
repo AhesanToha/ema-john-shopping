dfc240ed1387ca2fa45e9094207147b54f081396
Toha92 <ahesan868@gmail.com>
2022-04-03 20:12:42 +0600

    react router

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Cart.css";
 
-const Cart = ({ cart }) => {
-  console.log(cart);
+const Cart = (props) => {
+  const { cart } = props;
   let total = 0;
   let shipping = 0;
   let quantity = 0;
   for (let product of cart) {
     quantity = quantity + product.quantity;
-    total = total + product.price *product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   let tax = parseFloat((total * 0.1).toFixed(2));
@@ -21,6 +21,7 @@ const Cart = ({ cart }) => {
       <p>Total Shipping: $ {shipping}</p>
       <p>Tax: $ {tax}</p>
       <h5>Grand Total: $ {grandTotal.toFixed(2)}</h5>
+      {props.children}
     </div>
   );
 };
