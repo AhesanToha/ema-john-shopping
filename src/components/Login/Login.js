508b5c6bb100f0414d0927500f76c10379ee146f
Toha92 <ahesan868@gmail.com>
2022-04-15 14:45:18 +0600

    requireauth and shipping form added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, error] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
 
   const handleEmailBlur = (event) => {
     setEmail(event.target.value);
@@ -14,11 +22,19 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
+  const handleUserSignIn = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
   return (
     <div className="form-container">
       <div>
         <h2 className="form-title">Login</h2>
-        <form>
+        <form onSubmit={handleUserSignIn}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
@@ -39,6 +55,7 @@ const Login = () => {
               required
             />
           </div>
+          <p style={{ color: "red", fontWeight: "bold" }}>{error?.message}</p>
           <input className="form-submit" type="submit" value="Login" />
         </form>
         <p>
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -28,8 +28,8 @@ const Orders = () => {
       </div>
       <div className="cart-container">
         <Cart cart={cart}>
-          <Link to="/inventory">
-            <button>Proceed order</button>
+          <Link to="/shipment">
+            <button>Proceed Shipping</button>
           </Link>
         </Cart>
       </div>
